4ec36bb052ce2ff8768b027bd8e2db6749f0f60e
Paulo Carvalho <53330856+pcsales@users.noreply.github.com>
2022-09-27 13:59:59 -0300

    Add files via upload

---
+++ Atividade06uc10/ComaBem/src/pck_principal/Principal.java
@@ -0,0 +1,37 @@
+package pck_principal;
+
+public class Principal{
+    public static void main(String[] args){
+        DAO_Produtos obj_produtos = new DAO_Produtos();
+
+        //consultar produtos
+        //obj_produtos.consultar();
+
+        //Inserir Produtos
+        //Produto novoProduto = new Produto(7, "maca",25.00,12.00,1);
+        //obj_produtos.inserir(novoProduto);
+
+        //alterar produto
+        //Produto produtoAlterado = new Produto(7, "maca alterada", 27.00, 11.00, 2);
+        //obj_produtos.alterar(produtoAlterado);
+
+        //excluir produtos
+        //obj_produtos.excluir(7);
+
+        DAO_Unidades obj_unidades = new DAO_Unidades();
+
+        //consultar
+        //obj_unidades.consultar();
+
+        //inserir unidade
+        //Unidade novaUnidade = new Unidade(5,"unidade new");
+        //obj_unidades.inserir(novaUnidade);
+
+        //alterar unidade
+        //Unidade unidadeAlterada = new Unidade(5,"unidade new alterada");
+        //obj_unidades.alterar(unidadeAlterada);
+
+        //exluir unidade
+        //obj_unidades.excluir(5);
+    }
+}
